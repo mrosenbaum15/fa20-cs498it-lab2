6345a8040888aaa288725e3825f25a1899fd1b2a
mrosenbaum15 <44303950+mrosenbaum15@users.noreply.github.com>
2020-11-02 16:42:46 -0600

    Adding Arduino files

--- air_quality/air_quality.ino
+++ air_quality/air_quality.ino
@@ -8,24 +8,31 @@ void loop(){
     /* code for sending just the air quality 
     *  acts as proof of concept that AQ gets sent to Pi... run serial_read.py on the Pi to see AQ
     */
-    int airQuality;
-    airQuality = analogRead(0); // read from pin A0    
-    Serial.println(airQuality, DEC); // print numerical value from sensor        
-  //  Serial.println(" PPM"); // unit of air quality
-    delay(1000); // wait a second      
+//    int airQuality;
+//    airQuality = analogRead(0); // read from pin A0    
+//    Serial.println(airQuality, DEC); // print numerical value from sensor        
+//  //  Serial.println(" PPM"); // unit of air quality
+//    delay(1000); // wait a second      
 
     /* code or sending air quality and number of devices from wifi module
      * this is what you'll eventually need
+     * */
+     
     int airQuality;
     airQuality = analogRead(0); // read from pin A0    
     
-    if (Serial.available() > 0) {
-      int total_devices = Serial.read(); // gets number of devices sent from End Xbee
+    while (Serial.available() == 0 && Serial.read() != '\n') {
+    }
+   // int total_devices = Serial.read(); // gets number of devices sent from End Xbee
       // Serial.print("Total Devices: "); // probably don't need to transmit this serially to rasp pi xbee
       // Might need to modify next two lines to determine how you want to send devices and AQ
-      Serial.println(total_devices);
-      Serial.println(airQuality, DEC); // print numerical value from sensor        
-    }
+//    Serial.print("Dev: ");
+   // Serial.println(total_devices);
+//    Serial.print("AQ: ");
+    Serial.print(airQuality, DEC); // print numerical value from sensor
+    Serial.println(" PPM"); // print numerical value from sensor           
+    Serial.print(Serial.read());
+    Serial.println(" devices"); // print numerical value from sensor             
     delay(1000); // wait a second
-    */
+    
 }
